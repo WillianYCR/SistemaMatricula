8668bd843d7510ffb71bf94455687b833a69cb28
Alex-CG <a.contrerasgranados@gmail.com>
2014-12-08 20:08:24 -0500

    Quitando @Controller y usando SpringBeanAutowiringSupport para servlets

--- SistemaMatricula/src/main/java/dev/com/matricula/controller/AulaRegistrar.java
+++ SistemaMatricula/src/main/java/dev/com/matricula/controller/AulaRegistrar.java
@@ -1,67 +1,75 @@
 package dev.com.matricula.controller;
 
 import java.io.IOException;
-import java.util.List;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.swing.JOptionPane;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.context.support.SpringBeanAutowiringSupport;
 
-import javax.servlet.annotation.WebServlet;
-
-import dev.com.matricula.dao.AulaDao;
-import dev.com.matricula.daoimpl.AulaDaoImpl;
 import dev.com.matricula.model.Aula;
 import dev.com.matricula.service.AulaService;
-import dev.com.matricula.serviceimpl.AulaServiceImpl;
 
-@WebServlet(name = "AulaRegistrar", urlPatterns = {"/aulaRegistrar.do"})
-@Controller
+@WebServlet(name = "AulaRegistrar", urlPatterns = { "/aulaRegistrar.do" })
 public class AulaRegistrar extends HttpServlet {
-	
-	private static final long serialVersionUID = 1L;
-	@Autowired
-	private AulaService aulaService;
-	
-	public AulaRegistrar() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-	}
 
-	protected void doPost(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException {
-		//validar campos vacios
-				String tipo = request.getParameter("txtTipo");
-				String capacidad = request.getParameter("txtCapacidad");
-				if(tipo.equals("") || capacidad.equals("") || capacidad.length() > 5){
-					JOptionPane.showMessageDialog(null, "Ingrese valores correctos necesarios");
-					request.getRequestDispatcher("AulaRegistrar.jsp").forward(request, response);
-				}else{
-					short iCapacidad = Short.parseShort(capacidad);
-					Aula objAula = new Aula();
-					objAula.setTipo(tipo);
-					objAula.setCapacidad(iCapacidad);
-					JOptionPane.showMessageDialog(null, "objeto Tipo : " + objAula.getTipo() + "\n" + "objeto Capadidad: " +objAula.getCapacidad() );
-					//Enviar datos para alamcenar
-					AulaDaoImpl daoI = new AulaDaoImpl();
-					//if(aulaService.registrarAula(objAula)){
-					if(daoI.persistirAula(objAula)){	
-						JOptionPane.showMessageDialog(null, "Registro grabado correctamente");
-						request.getRequestDispatcher("AulaListar.jsp").forward(request, response);
-					}else{
-						JOptionPane.showMessageDialog(null, "Error al registrar el aula");
-						request.getRequestDispatcher("jsp/AulaRegistrar.jsp").forward(request, response);
-					}
-				}
-	}
+  private static final long serialVersionUID = 1L;
+  @Autowired
+  private AulaService aulaService;
+
+  // @Autowired
+  // private AulaDao daoI;
+
+  public AulaRegistrar() {
+    super();
+    // TODO Auto-generated constructor stub
+  }
+
+  @Override
+  public void init(ServletConfig config) throws ServletException {
+    super.init(config);
+    SpringBeanAutowiringSupport.processInjectionBasedOnServletContext(this,
+        config.getServletContext());
+  }
+
+  @Override
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+
+  }
+
+  @Override
+  protected void doPost(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+    // validar campos vacios
+    String tipo = request.getParameter("txtTipo");
+    String capacidad = request.getParameter("txtCapacidad");
+    if (tipo.equals("") || capacidad.equals("") || capacidad.length() > 5) {
+      JOptionPane.showMessageDialog(null, "Ingrese valores correctos necesarios");
+      request.getRequestDispatcher("AulaRegistrar.jsp").forward(request, response);
+    } else {
+      short iCapacidad = Short.parseShort(capacidad);
+      Aula objAula = new Aula();
+      objAula.setTipo(tipo);
+      objAula.setCapacidad(iCapacidad);
+      JOptionPane.showMessageDialog(null, "objeto Tipo : " + objAula.getTipo() + "\n"
+          + "objeto Capadidad: " + objAula.getCapacidad());
+      // Enviar datos para alamcenar
+      // if(aulaService.registrarAula(objAula)){
+      if (aulaService.registrarAula(objAula)) {
+        JOptionPane.showMessageDialog(null, "Registro grabado correctamente");
+        request.getRequestDispatcher("AulaListar.jsp").forward(request, response);
+      } else {
+        JOptionPane.showMessageDialog(null, "Error al registrar el aula");
+        request.getRequestDispatcher("jsp/AulaRegistrar.jsp").forward(request, response);
+      }
+    }
+  }
 
 }
